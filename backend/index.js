4f1d263fffe051d65c950767550fd3fcb736e13e
Hilina Zemedkun <169660585+hilinaz@users.noreply.github.com>
2025-04-11 00:35:52 +0300

    final-versio

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,22 @@ const app = express()
 const cors = require("cors")
 app.use(cors())
 const usersRoutes = require("./routes/userRoutes")
-const authMiddleWare = require(".authMiddleware.js");
+const authMiddleWare = require("./middleware/authMiddleware");
 PORT = 5500
 // const PORT = process.env.PORT || 5500
 const dbcon = require("./db/dbConfig")
 //login route
+app.use(express.static("public"));
 
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("done!")
+    res.sendFile(__dirname + "../public/index.html");
+
 })
+
+
+
 
 app.use("/api/users", usersRoutes)
 
@@ -33,7 +38,7 @@ async function start() {
 
 start()
 
-app.listen(PORT, (err) => {
+app.listen(3000, (err) => {
   if (err) {
     console.log(err.message)
   } else {
