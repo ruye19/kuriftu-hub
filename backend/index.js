dfd29baf257607a7fe4059785f93c52209b97d1d
ruthyeshitila12@gmail.com <167624441+ruye19@users.noreply.github.com>
2025-04-10 12:00:01 +0300

    login and register backend

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+//require("dotenv").config();
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(cors())
+const usersRoutes = require("./routes/userRoutes")
+
+const authMiddleWare = require("./middleware/AuthMiddleware")
+PORT = 5500
+// const PORT = process.env.PORT || 5500
+const dbcon = require("./db/dbConfig")
+//login route
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("done!")
+})
+
+app.use("/api/users", usersRoutes)
+
+
+
+async function start() {
+  try {
+    console.log("start database connection")
+    const result = await dbcon.execute("select 'test'")
+    console.log(result)
+    console.log("end database connection")
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+start()
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err.message)
+  } else {
+    console.log("server is up ")
+  }
+})
+
+
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router()
+
+// usercontroller
+const authMiddleWare = require("../middleware/authMiddleware")
+
+const { register, login, checkUser,getFullName } = require("../controller/userController")
+
+router.post("/register", register)
+router.post("/login", login)
+router.get("/check", authMiddleWare,checkUser)
+router.get("/getFullName", authMiddleWare, getFullName)
+
+module.exports = router
